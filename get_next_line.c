e90fdc059e7e1a555a2b9c49636fb4f355c0a953
Dorian Abdullahi <dabdulla@c2r1p9.42vienna.com>
2025-10-25 18:00:36 +0200

    added BUFFER_SIZE to header and learned how it works

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabdulla <dabdulla@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 19:09:55 by dabdulla          #+#    #+#             */
+/*   Updated: 2025/10/25 17:55:21 by dabdulla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+    return ("HEHEHEHEHE");
+}
+
+#include <stdio.h>
+int main(void)
+{
+    printf("%i",BUFFER_SIZE);
+}
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabdulla <dabdulla@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/23 19:09:57 by dabdulla          #+#    #+#             */
+/*   Updated: 2025/10/25 17:58:27 by dabdulla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE 
+# define BUFFER_SIZE 42
+#endif
+
+char	*get_next_line(int fd);
+#endif
